ffaf14c9f1b5a88971ba16545f9466e9ccea3d16
313yang <313yang@naver.com>
2025-03-31 14:03:37 +0900

    baekjoon-14681

--- Baekjoon/14681.js
+++ Baekjoon/14681.js
@@ -1,4 +1,4 @@
-const [x, y] = require('fs').readFileSync("input.txt", 'utf-8').trim().split('\n').map(Number);
+const [x, y] = require('fs').readFileSync(0, 'utf-8').trim().split('\n').map(Number);
 
 if (x > 0)
     y > 0 ? console.log(1) : console.log(4)
