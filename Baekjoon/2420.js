2092ae0ca71486e5a61497713a0058d37289c128
313yang <313yang@naver.com>
2025-03-31 16:32:28 +0900

    baekjoon-2420

--- Baekjoon/2420.js
+++ Baekjoon/2420.js
@@ -1,7 +1,4 @@
 const i = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 const a = +i[0];
 const b = +i[1];
-let r = 0
-a < 0 ? r += -a : r += a
-r += b
-console.log(r)
+console.log(Math.abs(a - b))
