f51eb99d1e863dd60066d536e2a3ff24cec3ce68
313yang <313yang@naver.com>
2025-04-02 08:49:29 +0900

    remove cached

--- index.js
+++
@@ -1,8 +0,0 @@
-const [x, y] = require('fs').readFileSync("input.txt", 'utf-8').trim().split('\n').map(Number);
-
-if (x > 0)
-    y > 0 ? console.log(1) : console.log(4)
-
-else if (x < 0)
-    y > 0 ? console.log(2) : console.log(3)
-
